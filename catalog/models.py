0511a2ae98fd1cafd59cc40465ab3377a6b06363
kirill <lobbi.kirill@mail.ru>
2023-08-08 22:48:25 +0300

    Для каждой моделей Product и Category описал поля согласно задания.

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=250, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    phot = models.ImageField(upload_to='photo/', **NULLABLE, verbose_name='Изображение (превью)')
+    # category = models.CharField(max_length=100, verbose_name='Категория')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    prise = models.FloatField(**NULLABLE, default=0, verbose_name='Цена за покупку')
+    date_create = models.DateField(**NULLABLE, verbose_name='Дата создания')
+    date_changes = models.DateField(**NULLABLE, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name} ({self.category})'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
