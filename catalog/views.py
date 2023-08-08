b29ed9ba79f5fb57fd2595232a594f80ec512f40
kirill <lobbi.kirill@mail.ru>
2023-08-09 02:04:33 +0300

    Дополнительное задание В контроллер отображения главной страницы добавил выборку последних 5 товаров и вывод их в консоль.

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
+from catalog.models import Product
 
 # Create your views here.
 def home(request):
+    my_prod = Product.objects.order_by('-id')[0:5]
+    print(my_prod)
     return render(request, 'catalog/home.html')
 
 
